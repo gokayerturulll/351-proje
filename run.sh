099b2b7120c424c574f9da3a07bee895868ab2c4
Gökay <gokay@Gokay-MacBook-Air.local>
2025-12-27 16:47:02 +0300

    Change Turkish text to English in run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # CSE351 DCE Project - Run Script
 
-echo "Derleniyor..."
+echo "Compiling..."
 make clean
 make
 
 echo ""
-echo "Test 1 çalıştırılıyor..."
+echo "Running Test 1..."
 ./dce < tests/test1.il
 
 echo ""
-echo "Test 2 çalıştırılıyor..."
+echo "Running Test 2..."
 ./dce < tests/test2.il
